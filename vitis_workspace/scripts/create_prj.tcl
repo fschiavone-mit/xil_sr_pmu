3adf423ca5ddeedea07b604a360e5b4723ef8fc4
frank <frank.schiavone@ll.mit.edu>
2024-07-24 17:16:38 -0400

    adding vitis project build script

---
+++ vitis_workspace/scripts/create_prj.tcl
@@ -0,0 +1,17 @@
+set WORK_DIR        [pwd]
+set VIVADO_ARTF_DIR ./../pl/artf/
+set XSA_NAME        xsa_24_07_24_16_07_50.xsa
+set XSA             ${VIVADO_ARTF_DIR}/${XSA_NAME}
+
+platform create -name {platform} \
+                -hw ${XSA} \
+                -proc {psu_cortexa53_0} -os {standalone} \
+                -arch {64-bit} \
+                -fsbl-target {psu_cortexa53_0} \
+                -out ${WORK_DIR}
+
+domain active standalone_domain
+bsp setlib -name xiltimer -ver 1.1
+bsp regenerate
+
+platform generate 
